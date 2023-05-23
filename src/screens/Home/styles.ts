c8181823b0d30a7c32ea4d7e9cf685b5dfc8948b
Roger Oliveira <rogerpolvr@gmail.com>
2023-05-23 01:40:13 -0300

    chore: project setup

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#ffff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 80,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+    padding: 10,
+  },
+  input: {
+    padding: 10,
+    backgroundColor: "#1f1E25",
+    height: 56,
+    borderRadius: 5,
+    color: "#ffff",
+  },
+  buttonText: {
+    padding: 10,
+    borderRadius: 5,
+    color: "#ffff",
+    fontSize: 24,
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: "#31cf67",
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
